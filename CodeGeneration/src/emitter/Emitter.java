f0fb048a26d6ea13e3063a561038c72e15d87516
sadhikamalladi <sadhika.malladi@outlook.com>
2014-11-13 17:11:58 -0800

    CodeGeneration lab

---
+++ CodeGeneration/src/emitter/Emitter.java
@@ -0,0 +1,35 @@
+package emitter;
+
+import java.io.*;
+
+public class Emitter
+{
+	private PrintWriter out;
+
+	//creates an emitter for writing to a new file with given name
+	public Emitter(String outputFileName)
+	{
+		try
+		{
+			out = new PrintWriter(new FileWriter(outputFileName), true);
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	//prints one line of code to file (with non-labels indented)
+	public void emit(String code)
+	{
+		if (!code.endsWith(":"))
+			code = "\t" + code;
+		out.println(code);
+	}
+
+	//closes the file.  should be called after all calls to emit.
+	public void close()
+	{
+		out.close();
+	}
+}
